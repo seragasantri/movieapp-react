1f3ca7cc34cd6dd2db42433798c8b73e61e0c374
seragasantri <seragasantri@gmail.com>
2023-09-21 15:24:08 +0700

    First Commit

---
+++ src/Api.js
@@ -0,0 +1,18 @@
+import { logDOM } from "@testing-library/react";
+import axios from "axios";
+
+export const getMovieList = async() => {
+    const movie = await axios.get(
+        `${process.env.REACT_APP_BASEURL}/movie/popular?api_key=${process.env.REACT_APP_APIKEY  }`
+    )
+    // console.log({movieList: movie});
+    return movie.data.results
+}
+
+export const searchMovie = async (q) => {
+    const search = await axios.get(
+        `${process.env.REACT_APP_BASEURL}/search/movie?query=${q}&api_key=${process.env.REACT_APP_APIKEY  }`
+    );
+    // console.log(q);
+    return search.data
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getMovieList, searchMovie } from "./Api";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
-function App() {
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+  useEffect(() => {
+    getMovieList().then((results) => {
+      setPopularMovies(results);
+    });
+  }, [])
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="movie-wrapper" key={i}>
+            <div className="movie-title">{movie.title}</div>
+            <LazyLoadImage  loading="lazy" className="movie-image" src={`${process.env.REACT_APP_BASEIMGURL}w500/${movie.poster_path}`} />
+            <div className="movie-date">Release: {movie.release_date}</div>
+            <div className="movie-rate">Rate: {movie.vote_average}</div>
+          </div>
+      )
+    })
+  }
+  const search = async (q) => {
+
+    const query = await searchMovie(q)
+    setPopularMovies(query.results)
+    // console.log({ query: query });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>REACT MOVIE APP</h1>
+        <input
+          className="movie-search"
+          placeholder="Cari Film Kesayangan ..."
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="movie-container">
+          <PopularMovieList/>
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
